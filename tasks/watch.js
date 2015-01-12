13cc5c6d0ae29bdcb4d67dd20e8374b9a249694a
Paul Love <paul@anglepoised.com>
2015-01-12 15:49:13 +0000

    Incorporate spriting tool
    Decided to call everything we're using icon rather than image and update paths to reflect.

--- tasks/watch.js
+++ tasks/watch.js
@@ -7,7 +7,7 @@ gulp.task('watch', ['default'], function () {
         paths = {
             dist: 'dist',
             srcHtml: 'src/**/*.html',
-            srcImages: 'src/assets/images/**/*.{gif,jpg,png,svg}',
+            srcIcons: 'src/assets/icons/**/*.{svg}',
             srcStyles: 'src/assets/styles/**/*.css',
             srcStylesLint: 'src/assets/styles/components/**/*.css',
             srcScripts: 'src/assets/scripts/**/*.js',
@@ -27,7 +27,7 @@ gulp.task('watch', ['default'], function () {
 
     // watch src gulp tasks and trigger reload
     gulp.watch(paths.srcHtml, {interval: 500}, ['html', 'html:lint', reload]);
-    gulp.watch(paths.srcImages, {interval: 500}, ['images', reload]);
+    gulp.watch(paths.srcIcons, {interval: 500}, ['icons', reload]);
     gulp.watch(paths.srcScripts, {interval: 500}, ['scripts', reload]);
     gulp.watch(paths.srcScriptsLint, {interval: 500}, ['scripts:lint']);
     gulp.watch(paths.srcStyles, {interval: 500}, ['styles', reload]);
